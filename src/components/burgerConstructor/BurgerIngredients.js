c97ffc1c65b6ea5f4dece319a8b8d5805b049d14
Yura Dudin <dudinyv@yandex.ru>
2022-06-02 23:43:27 +0500

    fix: BurgerIngredients

--- src/components/burgerConstructor/BurgerIngredients.js
+++ src/components/burgerConstructor/BurgerIngredients.js
@@ -1,66 +1,76 @@
 import React, { useState } from 'react';
-import style, { Tab, CurrencyIcon  } from '@ya.praktikum/react-developer-burger-ui-components';
+import style, { Tab, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import burgerIngredients from './burgerIngredients.module.css'
 
 export default function BurgerIngredients({ data }) {
-	const [current, setCurrent] = useState('one');
-	console.log(data);
 
-	const arr = data.map((item) =>
-		<div key={item._id}>
-			<h3>{item.name}</h3>
-			<ul>
-				<li className={burgerIngredients.burgerIngredients__item}>
-					<img src={item.image}></img>
-					<p>{item.price}</p>
-					<CurrencyIcon type="primary" />
-					<p>{item.name}</p>
-				</li>
-			</ul>
-		</div>
-	);
+	const hiddenElement = document.querySelector('.main');
+	console.log(hiddenElement);
 
+	const [current, setCurrent] = useState('bun');
+	// console.log(data);
 	return (
 		<>
 			<section className={burgerIngredients.burgerIngredients}>
-			<h2 className={` ${burgerIngredients.burgerIngredients__title} text text_type_main-large`}>Собери бургер</h2>
-					<div style={{ display: 'flex' }}>
-						<Tab value="one" active={current === 'one'} onClick={setCurrent}>
-							Булки
-						</Tab>
-						<Tab value="two" active={current === 'two'} onClick={setCurrent}>
-							Соусы
-						</Tab>
-						<Tab value="three" active={current === 'three'} onClick={setCurrent}>
-							Начинки
-						</Tab>
-					</div>
-					<h2>Булки</h2>
+				<h2 className={` ${burgerIngredients.burgerIngredients__title} text text_type_main-large`}>Собери бургер</h2>
+				<div style={{ display: 'flex' }}>
+					<Tab value="bun" active={current === 'bun'} onClick={setCurrent}>Булки</Tab>
+					<Tab value="sauce" active={current === 'sauce'} onClick={setCurrent}>Соусы</Tab>
+					<Tab value="main" active={current === 'main'} onClick={setCurrent}>Начинки</Tab>
+				</div>
+				<div className={burgerIngredients.burgerIngredients__ingredients}>
+					<h2 className="text text_type_main-medium">Булки</h2>
 					<div className={burgerIngredients.burgerIngredients__ingredient}>
-						
-							{
-								data.filter(card => card.type == 'bun').map(filteredType => (
-									<p key={filteredType._id}>{filteredType.name}</p>
-								))
-							}
-						
+						{
+							data.filter(card => card.type == 'bun').map(filteredType => (
+								<ul className={burgerIngredients.burgerIngredients__card} key={filteredType._id}>
+									<li className={burgerIngredients.burgerIngredients__item}>
+										<img className={burgerIngredients.burgerIngredients__image} src={filteredType.image}></img>
+										<div className={burgerIngredients.burgerIngredients__price}>
+											<p className={` ${burgerIngredients.burgerIngredients__cost} text text_type_digits-default`}>{filteredType.price}</p>
+											<CurrencyIcon type="primary" />
+										</div>
+										<p className={` ${burgerIngredients.burgerIngredients__name} text text_type_main-default`}>{filteredType.name}</p>
+									</li>
+								</ul>
+							))
+						}
 					</div>
-					<h2>Соусы</h2>
+					<h2 className="text text_type_main-medium">Соусы</h2>
 					<div className={burgerIngredients.burgerIngredients__ingredient}>
-					{
-								data.filter(card => card.type == 'sauce').map(filteredType => (
-									<p key={filteredType._id}>{filteredType.name}</p>
-								))
-							}
+						{
+							data.filter(card => card.type == 'sauce').map(filteredType => (
+								<ul className={burgerIngredients.burgerIngredients__card} key={filteredType._id}>
+									<li className={burgerIngredients.burgerIngredients__item}>
+										<img className={burgerIngredients.burgerIngredients__image} src={filteredType.image}></img>
+										<div className={burgerIngredients.burgerIngredients__price}>
+											<p className={` ${burgerIngredients.burgerIngredients__cost} text text_type_digits-default`}>{filteredType.price}</p>
+											<CurrencyIcon type="primary" />
+										</div>
+										<p className={` ${burgerIngredients.burgerIngredients__name} text text_type_main-default`}>{filteredType.name}</p>
+									</li>
+								</ul>
+							))
+						}
 					</div>
-					<h2>Начинка</h2>
+					<h2 className="text text_type_main-medium main">Начинка</h2>
 					<div className={burgerIngredients.burgerIngredients__ingredient}>
-					{
-								data.filter(card => card.type == 'main').map(filteredType => (
-									<p key={filteredType._id}>{filteredType.name}</p>
-								))
-							}
+						{
+							data.filter(card => card.type == 'main').map(filteredType => (
+								<ul className={burgerIngredients.burgerIngredients__card} key={filteredType._id}>
+									<li className={burgerIngredients.burgerIngredients__item}>
+										<img className={burgerIngredients.burgerIngredients__image} src={filteredType.image}></img>
+										<div className={burgerIngredients.burgerIngredients__price}>
+											<p className={` ${burgerIngredients.burgerIngredients__cost} text text_type_digits-default`}>{filteredType.price}</p>
+											<CurrencyIcon type="primary" />
+										</div>
+										<p className={` ${burgerIngredients.burgerIngredients__name} text text_type_main-default`}>{filteredType.name}</p>
+									</li>
+								</ul>
+							))
+						}
 					</div>
+				</div>
 			</section>
 		</>
 	);
