5be03c0760a1c719febd377d4818fcc03065b843
Yura Dudin <dudinyv@yandex.ru>
2022-10-03 23:04:33 +0500

    add: register.js

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,10 +1,10 @@
 
 import { useState } from 'react';
-import style, { EmailInput, PasswordInput, Button } from '@ya.praktikum/react-developer-burger-ui-components';
+import style, { EmailInput, PasswordInput, Input, Button } from '@ya.praktikum/react-developer-burger-ui-components';
 import { Link } from 'react-router-dom';
 import login from './login.module.css';
 
-export function Login () {
+export function Register () {
 
   const [email, setEmail] = useState('test@yandex.ru');
   const [value, setValue] = useState('password');
@@ -16,19 +16,15 @@ export function Login () {
 
   return (
     <div className={login.login}>
-      <h2 className='text text_type_main-medium'>Вход</h2>
+      <h2 className='text text_type_main-medium'>Регистрация</h2>
       <div className={login.email}>
         <EmailInput onChange={onChange} value='' name={'email'} />
       </div>
       <PasswordInput onChange={onChange} value='' name={'password'} />
       <div className={login.button}>
         <Button type="primary" size="medium">
-          Войти
+          Зарегистрироваться
         </Button>
-      </div>
-      <div className={`${login.newUser} text text_type_main-default text_color_inactive`}>
-        <p className={login.paragraf}>Вы — новый пользователь?</p>
-        <Link to="/">Зарегистрироваться</Link>
       </div>
       <div className={`${login.passwordRecovery} text text_type_main-default text_color_inactive`}>
         <p className={login.paragraf}>Забыли пароль?</p>
