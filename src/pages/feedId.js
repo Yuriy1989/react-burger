17955c4ed8188c9420fdac1a3dd689adb127e2b8
Yura Dudin <dudinyv@yandex.ru>
2022-11-21 23:24:55 +0500

    add: time

--- src/pages/feedId.js
+++ src/pages/feedId.js
@@ -7,23 +7,26 @@ import uuid from 'react-uuid';
 
 export function FeedId () {
 
-  let todayDate = new Date();
-  let currentTimeZoneOffsetInHours  = todayDate.getTimezoneOffset() / 60;
+  // let todayDate = new Date();
+  // let currentTimeZoneOffsetInHours  = todayDate.getTimezoneOffset() / 60;
+  // let test = todayDate.toISOString();
+  // console.log('toda', test);
 
   const { id }  = useParams(); //id бургера
   const card = useSelector(state => state.orders.orders); //последние 50 заказов
   const ingredientsData = useSelector(state => state.getIngredientsApi.ingredientsGetApi); //все возможные ингредиенты
   const [cellOrder, setCellOrder] = useState(0); //цена за бургер
-  const [data, setData] = useState([]);
-  const [filterData, setFilterData] = useState([]);
-  const [burger, setBurger] = useState([]); //готовый бургер
+  const [data, setData] = useState([]); //название, номер и цена бургера
+  const [filterData, setFilterData] = useState([]); //бургер со всеми ингредиентами и подробной информацией
+  const [burger, setBurger] = useState([]); //готовый бургер с фильтром по уникальным ингредиентам
   const [count, setCount] = useState({}); //объект типа {ID:количество} в массиве countData
 
-  console.log('ingredientsData', ingredientsData);
-  console.log('card', card);
-  console.log('id', id);
-  console.log('filterData', filterData);
-  console.log('count', count);
+  // console.log('ingredientsData', ingredientsData);
+  // console.log('card', card);
+  // console.log('id', id);
+  // console.log('filterData', filterData);
+  // console.log('burger', burger);
+
 
   //Ищем ингредиент из общего массива ингредиентов по определенному id из ссылки
 
@@ -67,6 +70,51 @@ export function FeedId () {
     const createBurger = [...uniqueMas, ...bun]; //добавляем булку в конец массива
 
     setBurger(createBurger); //передаем ингредиенты с подробной информацией в state
+
+
+    //расчет времени
+    const minute = 1000 * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+    const year = day * 365;
+    let timeCreate = null;
+
+    let todayDate = new Date();
+
+    let currentTimeZoneOffsetInHours  = todayDate.getTimezoneOffset() / 60;
+    let test = todayDate.toISOString();
+    // console.log('today', Date.parse(test));
+    const t = Date.parse(itemBurger?.createdAt);
+    const d = Date.parse(test);
+    let days = Math.round((d-t) / day);
+    console.log('days',  days);
+    console.log('t', d-t);
+    console.log('d', d);
+
+    days = 1 ? 'Сегодня' : 'Вчера';
+
+    const event = new Date(todayDate);
+    const createBurg = new Date(itemBurger?.createdAt);
+
+    const r = createBurg;
+    console.log('r', r);
+    var pos = r.indexOf("locate");
+
+
+    console.log('time = ', pos);
+
+    console.log('event', event);
+    (itemBurger?.createdAt) ? console.log('createBurg', createBurg) : '';
+    console.log(createBurg.getHours());
+    console.log(createBurg.getMinutes());
+    console.log(createBurg.getTimezoneOffset() / 60);
+
+    timeCreate = days = 1 ? 'Сегодня' : 'Вчера';
+
+
+    console.log('timeCreate', timeCreate + ', ' + createBurg.getHours() + ':' + createBurg.getMinutes());
+    // console.log('itemBurger', itemBurger?.createdAt);
+
   }
 
   useEffect(() => {
@@ -77,7 +125,7 @@ export function FeedId () {
     <div className={feedId.feedId}>
       <h2 className={` ${feedId.idOrder} text text_type_digits-default `}># {data?.number}</h2>
       <p className={` ${feedId.name} text text_type_main-medium `}>{data?.name}</p>
-      <p className={` ${feedId.status} text text_type_main-default `}>{data?.status}</p>
+      <p className={` ${feedId.status} text text_type_main-default `}>{(data?.status === 'done') ? 'Выполнен' : 'В работе'}</p>
       <p className={` ${feedId.ingredients} text text_type_main-medium `}>Состав:</p>
       <ul className={` ${feedId.ingredient} `}>
         {burger.map(item => (
@@ -89,7 +137,7 @@ export function FeedId () {
               <p className='text text_type_main-default mr-4 ml-4'>{item.name}</p>
             </div>
             <div className={` ${feedId.item} `}>
-              <p className={`text text_type_digits-default`}>{count[item.id] > 1 ? (count[item.id]) : '1'}</p>
+              <p className={`text text_type_digits-default`}>{(count[item.id] > 1 && item.type != 'bun') ? (count[item.id]) : '1'}</p>
               <p className='text text_type_digits-default mr-2'>&nbsp;x</p>
               <p className='text text_type_digits-default mr-2'>{`${item.price}`}</p>
               <CurrencyIcon type="primary" />
@@ -98,7 +146,7 @@ export function FeedId () {
         ))}
       </ul>
       <div className={feedId.cellPrice}>
-          <p className={`text text_type_main-default text_color_inactive`}>Сегодня, {todayDate.getHours()}:{todayDate.getMinutes()} i-GMT{currentTimeZoneOffsetInHours}</p>
+          {/* <p className={`text text_type_main-default text_color_inactive`}>Сегодня, {todayDate.getHours()}:{todayDate.getMinutes()} i-GMT{currentTimeZoneOffsetInHours}</p> */}
           <div className={feedId.cell}>
             <p className={`text text_type_digits-default mr-2`}>{cellOrder}</p>
             <CurrencyIcon type="primary" />
